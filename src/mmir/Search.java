9ba87c55756d0ec5d4081e617eb0c216cd8af966
SunPHM <pengyang823@gmail.com>
2015-10-21 23:58:01 -0400

    add the similarity score function

--- src/mmir/Search.java
+++ src/mmir/Search.java
@@ -1,7 +1,12 @@
 package mmir;
 
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.HashMap;
 
 import org.apache.solr.client.solrj.SolrQuery;
@@ -9,6 +14,7 @@ import org.apache.solr.client.solrj.SolrServerException;
 import org.apache.solr.client.solrj.impl.HttpSolrServer;
 import org.apache.solr.client.solrj.response.QueryResponse;
 import org.apache.solr.common.SolrDocumentList;
+import org.apache.solr.common.SolrInputDocument;
 
 /**
  * Indexing and Searching runs locally using Solr
@@ -16,63 +22,44 @@ import org.apache.solr.common.SolrDocumentList;
 
 public class Search {
 	
-	public static int featureSize = 128;
-	
-	public static int clusterNum = 100;
-	public static int topclusterNum = 10;
-	public static int botclusterNum = 10;
-	public static String clusters = "test/cluster/clusters/";
-	public static String terms = "data/features/frequency.txt";
-	
 	public static String urlString = "http://localhost:8983/solr";
 	public static int numOfResults = 20;
-	
-	//cache 
-	static double[][] topcluster = null;
 	
 	static HashMap<Integer, double[][]> botclusters = new HashMap<Integer, double[][]>();
 	static final int max_number_botclusters = 200; //200 cache to store the bot level clusters
 	//static int index_to_be_deleted = 0; // the  clusters to be deleted when hashmap is full
 	
 	public static void main(String[] args) throws IOException, SolrServerException{
-		// run indexing
-		//runIndexing("test/data/frequency.txt");
-		//search("data/images/all_souls_000000.jpg");
-//		loadConfiguration("test/conf.xml");
-		//loadConfiguration_topdown("test/conf_new.xml");
-		//System.out.println(terms);
-		//System.out.println(clusters);
-		//System.out.println(clusterNum);
-		//Search.loadConfiguration_topdown("test/conf_new.xml");
-		//Search.search_topdown("data/images/all_souls_000000.jpg");
-		//test("/home/yp/Desktop/results/test14");
+		search("data/query/google_query.txt");
 	}
 	
-	
-	public static void init(String terms, int clusterNum, String clusters, int topclusterNum, int botclusterNum){
-		Search.terms = terms;
-		Search.clusterNum = clusterNum;
-		Search.clusters = clusters;
-		Search.topclusterNum = topclusterNum;
-		Search.botclusterNum = botclusterNum;
+	public static void search(String filename) throws IOException, SolrServerException{
 		
-		//init the static cache files -- important
-		Search.topcluster =null;
-		Search.botclusters.clear();
+		BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(filename)));
+		ArrayList<String> queries = new ArrayList<String>();
+		ArrayList<String> words = new ArrayList<String>();
+		String line;
+		while((line = br.readLine()) != null){
+			words.add(line.split("\t")[0]);
+			queries.add(line.split("\t")[1]);
+			//System.out.println(line);
+		}
+		br.close();
+		String[] results = query(queries.get(0), 20);
+		//for(String s : results)
+		//	System.out.println(s);
 	}
-	
 	
 	//query with customed number of results
 	public static String[] query(String s, int num_results) throws SolrServerException{//query and output results
-		
 		//query a numeric vector as a string
-		String urlString = Search.urlString;
 		HttpSolrServer server = new HttpSolrServer(urlString);
 		// search
 	    SolrQuery query = new SolrQuery();
 	    //query.setQuery("includes:" + s);
 	    query.set("q", "includes:" + s);
 	    query.setRows(num_results);
+	    query.set("fl","*,score");
 	    // get results		
 	    QueryResponse qresponse = server.query(query);
 	    SolrDocumentList list = qresponse.getResults();
@@ -80,6 +67,7 @@ public class Search {
 	    for(int i = 0; i < list.size(); i++){
 	    	//System.out.println(list.get(i).getFieldValue("id"));
 	    	files[i] = list.get(i).getFieldValue("id").toString();
+	    	System.out.println(list.get(i).getFieldValue("score"));
 	    }
 	    
 	    return files;
